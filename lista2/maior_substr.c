f4768302f65bbf84571cfb9b8aa60e935fda3a5a
Alexandre Beck <alexandrebeck2003@gmail.com>
2023-10-24 18:07:19 -0300

    accpeted

--- lista2/maior_substr.c
+++ lista2/maior_substr.c
@@ -1,48 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-// Encontra a primeira ocorrência de sub em str começando da posição start
-int findFirst(char *str, char *sub, int start)
+int Sub(char *str, char *sub)
 {
-    if (strstr(str + start, sub) == NULL)
-        return -1;                         // Se não houver ocorrência, retorna -1
-    return strstr(str + start, sub) - str; // Retorna a posição da primeira ocorrência
-}
+    char *auxiliar = malloc(strlen(str));
+    if (strlen(str) < strlen(sub))
+        return 0;
 
-// Encontra a próxima ocorrência de sub em str depois da posição start
-int findNext(char *str, char *sub, int start)
-{
-    if (strstr(str + start + 1, sub) == NULL)
-        return -1;
-    return strstr(str + start + 1, sub) - str;
-}
+    if (strcmp(strncpy(auxiliar, str, strlen(sub)), sub) == 0)
+    {
+        if (strcmp(strncpy(auxiliar, str + strlen(str) - strlen(sub), strlen(str)), sub) == 0)
+        {
+            return strlen(str);
+        }
+        else
+        {
+            return Sub(strncpy(auxiliar, str, strlen(str) - 1), sub);
+        }
+    }
 
-int maxSubstring(char *str, char *sub)
-{
-    int first = findFirst(str, sub, 0);
-    if (first == -1)
-        return 0; // Se não há ocorrência de sub, retorna 0
-
-    int next = findNext(str, sub, first);
-    if (next == -1)
-        return strlen(sub); // Se há apenas uma ocorrência, retorna o tamanho de sub
-
-    // Calcula o tamanho da substring entre first e next
-    int currentLength = next + strlen(sub) - first;
-
-    // Chama a função recursivamente para o restante da string depois de "first"
-    int remaining = maxSubstring(str + first + 1, sub);
-
-    if (currentLength > remaining)
-        return currentLength;
-    return remaining;
+    return Sub(strncpy(auxiliar, str + 1, strlen(str)), sub);
 }
 
 int main()
 {
-    char str[101], sub[101];
-    scanf("%s", str);
-    scanf("%s", sub);
-    printf("%d\n", maxSubstring(str, sub));
+    char str[101], sub[100];
+    scanf("%s %s", str, sub);
+    str[strlen(str)] = '\0';
+    sub[strlen(sub)] = '\0';
+
+    printf("%d\n", Sub(str, sub));
     return 0;
 }
